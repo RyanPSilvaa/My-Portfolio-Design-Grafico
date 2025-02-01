2a1897aa9bfa37aba674c8551ba8966c00620919
Ryan Pereira da Silva <166587298+RyanPSilvaa@users.noreply.github.com>
2025-02-01 00:47:14 -0400

    Atualizei tudo

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ let navbar = document.querySelector('.nav-bar');
 
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
+    navbar.classList.toggle('.active');
 }
 
 window.onscroll = () => {
     menu.classList.remove('bx-x');
-    navbar.classList.remove('active');
+    navbar.classList.remove('.active');
 }

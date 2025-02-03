17de44c4d8ea00d484bab22d223492ed282227f0
Ryan Pereira da Silva <166587298+RyanPSilvaa@users.noreply.github.com>
2025-02-02 21:14:03 -0400

    Atualizei o portfolio

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
+
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.nav-bar');
 
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
-    navbar.classList.toggle('.active');
+    navbar.classList.toggle('active');
 }
 
 window.onscroll = () => {
     menu.classList.remove('bx-x');
-    navbar.classList.remove('.active');
+    navbar.classList.remove('active');
 }
 
 const typed = new Typed('.multiple-text', {

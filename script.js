3c0df31bde3e86799b20c415e3b255d06e396ad6
Ryan Pereira da Silva <166587298+RyanPSilvaa@users.noreply.github.com>
2025-02-02 21:55:55 -0400

    Atualizei o meu portfolio ajeitando o menu responsivo

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
-
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.nav-bar');
-
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navbar.classList.toggle('.active');
-}
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll('.section');
+let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
-    menu.classList.remove('bx-x');
-    navbar.classList.remove('.active');
-}
+    let top = window.scrollY;  // Movi para fora do loop para otimizar
+
+    sections.forEach(sec => {
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active'); // Remove a classe 'active' de todos os links
+            });
+            // Aqui a busca é pelo link com o 'href' que corresponde ao id da seção
+            document.querySelector('header nav a[href*="' + id + '"]').classList.add('active');
+        }
+    });
+};
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
 
 const typed = new Typed('.multiple-text', {
     strings: ['Designer Gráfico', 'Design Web', 'Dev Front-End'],
